9799ed903218688db1da13c5dab4465726e4795b
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-07-02 08:35:00 +0530

    Made route settings and installed EJS as template engine

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
-
+const homeRoute = require('./routes/home')
 const app = express()
 
 require('dotenv').config({ path: './config/.env' })
+
+app.set('view engine', 'ejs')
+
+app.use('/', homeRoute)
 
 const PORT = process.env.PORT
 
